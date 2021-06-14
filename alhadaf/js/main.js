53cc3ec56065baeed84b5b61a53d87b3a73812b4
hadeermeshref <hadeer.magdy711@yahoo.com>
2021-06-14 09:59:27 +0200

    Initial commit

---
+++ alhadaf/js/main.js
@@ -0,0 +1,14 @@
+// skitter slider plugin
+$(function() {
+    $('.skitter-large').skitter({
+       
+    });
+  });
+
+  // loading screen
+  $(document).ready(function () {
+    $("#loading").fadeOut(1000,function(){
+      $("body").css("overflow","auto")
+
+    })
+  })
