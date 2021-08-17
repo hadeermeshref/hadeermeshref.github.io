e11850a6c0b9298d77bf706b9409b7b294089373
hadeermeshref <hadeer.magdy711@yahoo.com>
2021-08-17 22:31:54 +0200

    Initial commit

--- quani/js/main.js
+++ quani/js/main.js
@@ -1,37 +1,13 @@
 //fixed icons(whatsapp & call)
-$(document).ready(function () 
-{
-
-
-let aboutOffsetTop  = $("#about").offset().top;
-
-
-$(window).scroll(function () {
-
-  let wScroll =  $(window).scrollTop();
-
-  if(wScroll > aboutOffsetTop - 50)
-  {
-   
-    $("#btnUp").fadeIn(500);
-    $("#btnCall").fadeIn(500);
-  }
-  else
-  {
-   
-    $("#btnUp").fadeOut(500);
-    $("#btnCall").fadeOut(500);
-  }
- 
-
-
-})
+$(document).ready(function (){
  // loading screen
 $("#loading").fadeOut(1000,function(){
        $("body").css("overflow","auto")
 
 })
+
 })
+
 
 
 
